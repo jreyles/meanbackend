f755c75aa71c2163b12e01f27897579935ea8178
Jonathan Reyles <jreyles@gmail.com>
2016-03-18 23:23:52 -0700

    meanPrac

--- meanPrac/ViewController.swift
+++ meanPrac/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Foundation
+//import SocketIOClientSwift
 
 class ViewController: UIViewController {
+    // use your Heroku acccount as the socketURL
+    // if you didn't use Heroku your link would be http://localhost:3000
+//    let socket = SocketIOClient(socketURL: NSURL(fileURLWithPath: "http://localhost:5000")!, options: [.Log(true), .ForcePolling(true)]))
 
+    let socket = SocketIOClient(socketURL: NSURL(string: "http://192.168.1.48:3000")!, options: [.Log(true), .ForcePolling(true)])
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(socket)
+        print("hello")
+        print(socket.connect())
+        socket.on("connect") { data, ack in
+            print("iOS::WE ARE USING SOCKETS!")
+        }
+        socket.connect()
     }
-
+    
+    @IBAction func yellowButtonPressed(sender: UIButton){
+        print("javascript")
+        socket.emit("javascript")
+    }
+    @IBAction func orangeButtonPressed(sender: UIButton){
+        print("swift")
+        socket.emit("swift")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname, './static')));
+app.use(bodyParser.urlencoded());
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 3000));
+app.get('/', function(req, res) {
+ res.render('index');
+});
+
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port', app.get('port'));
+});
+var io = require('socket.io').listen(server);
+// var socket = io.sockets;
+
+io.sockets.on('connection', function (socket) {
+  console.log('SERVER::WE ARE USING SOCKETS!');
+  console.log(socket.id);
+
+socket.on("javascript", function(data) {
+    console.log("a vote for javascript");
+    io.sockets.emit("update_javascript");
+});
+
+socket.on("swift", function(data) {
+    console.log("a vote for swift");
+    io.sockets.emit("update_swift");
+});
+
+
+});
+//socket.connect();
